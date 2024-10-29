67b7675ccb9083cb0dba8c352bd1d8d3784e643d
Ayesha Khan <ayeshakhanak26k@gmail.com>
2024-10-28 19:10:35 -0700

    first commit

---
+++ assignments.js
@@ -0,0 +1,115 @@
+
+// Chapt 2
+// let c = alert("Hello World", "<i>");
+// let a = alert("Name: Ayesha Khan");
+// let b = alert( "Age: 18");
+// let d = alert("Course : Web And App Development"); 
+// let e = alert ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+// let f = alert ("My email:ayeshakhan@gmail.com");
+// let book = alert ("I am trying to learn from the Book A smarter way to learn Javascript");
+// let o = alert("(-------- ✿✿---------)")
+// let v = "Yah! I can write HTML content through JavaScript"
+// document.write(v);
+//Chap 3
+// let a = alert("I am 18 years old");
+// let b = alert("You have visited this site 14 times");
+// let c = "My Birth year is 2006"
+// document.write(c, "<br>")
+// let d = "Data type of my declared variable is number"
+// document.write(d  ,"<br>")
+// let e = ("John Doe orderes 5 T-Shirt(s) on XYZ Clothing Brand")
+// document.write(e , "<br>")
+//chap 4
+// let a = "<b> Rules for naming JS variables </b> <br> <br> ";
+// document.write(a ,"<br>");
+
+// let b = "Variables names can only contain numbers, $ and _. For example : $my_1stVariable"
+// document.write(b , "<br>")
+// let c = "Variables must begin with letter, $ or _. For example : $name, _name or name"
+// document.write(c , "<br>")
+// let d = "Variable names are case sensitive <br>" 
+// document.write(d)
+// let e ="Variable names should not be JS keywords"
+// document.write(e)
+//chp 5
+// let a = "15";
+// let b  = "6";
+// a = parseInt(a);
+// b = parseInt(b);
+// console.log("Sum of two Variable is" , a+b); 
+// console.log("Sum of two Variable is" , a-b); 
+// console.log("Sum of two Variable is" , a*b); 
+// console.log("Sum of two Variable is" , a/b); 
+// console.log("Sum of two Variable is" , a%b); 
+
+// console.log("Value after decleration is");
+//  let d = 6;
+// console.log("Initial Value = 6" )
+// console.log("After Increment =" , ++d)
+// // console.log( "After Decrement=" , --d)
+// // console.log( "After Adding 7 to the variable= " , d+7)
+// // console.log ("Decrement after addition =" , d-- )
+// d = d +7;
+// console.log("value after adddition = " , d );
+// d--
+// console.log (" Value after decrement =" , d);
+// console.log ("The Remainder is:" , 6%3);
+//next question
+// let j  = 600;
+// console.log("Cost of One movie Ticket is = " , j );
+// console.log ("5 ticket price=" , j*5);
+// document.write("The cost to buy 5 tickets for a movie is <b>3000PKR</b>")
+// //Table Generator 
+// let table = prompt("Enter The table you want")
+// for( let i = 1; i<=10 ; i++) {
+// document.write (table + " X " + i + "= " + table*i + "<br>")
+// }
+// shopping cart
+// let a = prompt("Price of item 1 =")
+// a = parseInt(a)
+// console.log("Price of item 1 =" , a )
+// let c = prompt("Price of item 2 =")
+//  c = parseInt(c)
+// console.log("Price of item 2 =" , c )
+
+// let b = prompt ("Quantity of item 1 =")
+
+// console.log("Quantity of item 1 =" , b )
+// let d = prompt ("Quantity of item 2 =")
+// console.log("Quantity of item  2 =" , d)
+// let e = 250
+//  e = parseInt(e)
+// console.log ("Shipping Charges for all over Pakistan is =" , e)
+// console.log("Total Cost of your order is =" , a+c*b*d+e);
+//Masksheet
+// let a = 980;
+// console.log("Total Marks =" , a)
+// let b = 804;
+// console.log("Obtained Marks =" , b)
+// console.log("Percentage = " , b*100/a)
+
+//Currency IN PKR
+// let a = 60;
+// console.log( "The Currency in PKR is :" , a+5*10/2)
+//AGE CALCULATOR:
+// let currentyear = 2024;
+// document.write("Current Year : 2024 <br>")
+// let birthyear = 1992 ;
+// document.write("Birth Year : 1992 <br>")
+// document.write ( " Your age: " , currentyear-birthyear )
+// The Geometrizer
+// let raduis = 20;
+// console.log("The Radius is : 20")
+// console.log("The Circumference is :"  , 2*3.14*raduis)
+// console.log("Area : " , 3.142*20**2 )
+//THE SUPPLY:
+// let snack = "Chips";
+// document.write("Favourite Snack : " , snack ,  "<br>")
+// let age = 18;
+// document.write("My Age  : " ,  age ,  "<br>")
+// let maxAge = 65;
+// document.write("Maximum Age  : " , maxAge ,  "<br>")
+// let perday = 3;
+// document.write("Per day intake : " , perday , "<br>")
+// document.write( " You will need snacks for the rest of your life is :" , perday*(maxAge-age) )
+
